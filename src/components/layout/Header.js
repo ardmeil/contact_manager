654ab6c45681ccba25298b766bcb3c9cf25f0e68
feildrixliemdra <feildrix.liemdra@ralali.com>
2019-06-30 22:47:33 +0700

    fixing header route

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,7 +5,7 @@ function Header(props) {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
                 <div className="container">
-                    <a href="/" className="navbar-brand">
+                    <a href="https://ardmeil.github.io/contact_manager/" className="navbar-brand">
                         {name}
                     </a>
                 <div>
--- src/pages/About.js
+++ src/pages/About.js
@@ -4,8 +4,8 @@ function About() {
     return (
         <div>
             <h1 className="display-4">About Contact Manager</h1>
-            <p className="lead">Simple app to manage contacts created with ReactJS</p>
-            <p className="text-secondary">Version 1.0.0</p>
+            <p className="lead">Simple app to manage contacts</p>
+            <p className="text-secondary">Created with ReactJS</p>
         </div>
     )
 }
